d89db91742b191dab7131c449a6686c6cc3f7ce2
Sam Watts <sam@seatfrog.com>
2022-05-26 15:29:17 +0100

    Added SPM package definition

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+	name: "Pilgrim-DI",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10)
+    ],
+	products: [
+		.library(name: "Pilgrim-DI", targets: ["Pilgrim"])
+	],
+	targets: [
+		.target(
+            name: "Pilgrim",
+            dependencies: [],
+            path: "Pilgrim"
+        ),
+		.testTarget(
+            name: "PilgrimTests",
+            dependencies: ["Pilgrim"],
+            path: "PilgrimTests"
+        )
+	],
+    swiftLanguageVersions: [.v5]
+)
